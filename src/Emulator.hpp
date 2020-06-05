e6f00fee65c3907a04efb98a335a3744c5c646ec
Jaron Bialecki <jaronb@Jarons-MacBook-Pro.local>
2020-06-05 16:48:52 -0400

    added opcode and register enums

---
+++ src/Emulator.hpp
@@ -0,0 +1,15 @@
+enum Register { R0 = 0, R1, R2, R3, R4, 
+                R5, R6, R7, PC, COND, COUNT };
+
+enum OpCode { BR = 0, ADD, LD, ST, 
+              JSR, AND, LDR, STR, 
+              RTI, NOT, LDI, STI,
+              JMP, RES, LEA, TRAP };
+
+class Emulator {
+    private:
+        uint16_t registers[11]
+
+    public:
+
+}

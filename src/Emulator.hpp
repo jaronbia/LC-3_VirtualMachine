fa78e279835787844f31c97e1a37737aa05d0cf6
Jaron Bialecki <jaronb@Jarons-MacBook-Pro.local>
2020-06-06 16:52:38 -0400

    added conditional flags and changed register name to reg

--- src/Emulator.hpp
+++ src/Emulator.hpp
@@ -5,10 +5,14 @@ enum OpCode { BR = 0, ADD, LD, ST,
               JSR, AND, LDR, STR, 
               RTI, NOT, LDI, STI,
               JMP, RES, LEA, TRAP };
+              
+enum CondFlags {  FL_POS = 1 << 0, /* P */
+                  FL_ZRO = 1 << 1, /* Z */
+                  FL_NEG = 1 << 2, /* N */ };
 
 class Emulator {
     private:
-        uint16_t registers[11]
+        uint16_t reg[11];
 
     public:
 

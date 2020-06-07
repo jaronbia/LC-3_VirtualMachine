325cab5adc1fad9c516d5a7b49cd36c7e0ea050b
Jaron Bialecki <jaronb@Jarons-MacBook-Pro.local>
2020-06-07 03:00:56 -0400

    added most of the operators, store still needs to be added. Testing should be done on all opcodes to ensure they work correct.

--- src/Emulator.cpp
+++ src/Emulator.cpp
@@ -3,8 +3,7 @@
 void Emulator::
 run() {
     bool active = true;
-    uint16_t instr;
-    uint16_t op;
+    uint16_t instr, op;
     // load args
     // setup
 
@@ -14,26 +13,103 @@ run() {
         instr = mem_read(reg[PC]++);
         op = instr >> 12;
 
-        if(op == ADD) add(instr);
-        else cerr << "Error: Invalid command encountered" << '\n';
+        if(op == ADD)      add(instr);
+        else if(op == LDI) ldi(instr);
+        else if(op == BR)  branch(instr);
+        else if(op == JMP) jump(instr);
+        else if(op == JSR) jsr(instr);
+        else if(op == LD)  load(instr);
+        else if(op == LDR) loadr(instr);
+        else if(op == LEA) loadea(instr);
+        else abort(); //cerr << "Error: Invalid command encountered" << '\n';
     }
     // shutdown
 }
 
 void Emulator::
-add(int instr) {
-    bool immFlag = (instr >> 5) & 0x1;      // are we in immediate mode? (originally uint16_t)
-    uint16_t r0 = (instr >> 9) & 0x7;       // destination register
-    uint16_t r1 = (instr >> 6) & 0x7;       // first operand
+add(const int instr)  {
+    uint16_t r0 = (instr >> 9) & 0x7;                    // destination register
+    uint16_t r1 = (instr >> 6) & 0x7;                    // first operand
+    uint16_t val = isImmMode(instr) ? signExtend(instr & 0x1F, 5) // if immediate
+                                    : reg[instr & 0x7];           // else register value
 
-    if(immFlag) {
-        uint16_t imm5 = signExtend(instr & 0x1F, 5);
-        reg[r0] = reg[r1] + imm5;
-    }
-    else {
-        uint16_t r2 = instr & 0x7;
-        reg[r0] = reg[r1] + reg[r2];
-    }
-
+    reg[r0] = reg[r1] + val;
     updateFlag(r0);
+}
+
+void Emulator::
+ldi(const int instr)  {
+    uint16_t r0 = (instr >> 9) & 0x7;                   // destination register
+    uint16_t r1 = (instr >> 6) & 0x7;                   // first operand
+
+    reg[r0] = mem_read(mem_read(reg[PC] + pcoffset(instr)));
+    updateFlag(r0);
+}
+
+void Emulator::
+band(const int instr) {
+    uint16_t r0 = (instr >> 9) & 0x7;                    // destination register
+    uint16_t r1 = (instr >> 6) & 0x7;                    // first operand
+    uint16_t val = isImmMode(instr) ? signExtend(instr & 0x1F, 5) // if immediate
+                                    : reg[instr & 0x7];           // else register value
+
+    reg[r0] = reg[r1] & val;
+    updateFlag(r0);
+}
+
+void Emulator:: 
+bnot(const int instr) {
+    uint16_t r0 = (instr >> 9) & 0x7;                    // destination register
+    uint16_t r1 = (instr >> 6) & 0x7;                    // first operand
+
+    reg[r0] = ~reg[r1];
+    updateFlag(r0);
+}
+
+void Emulator::
+branch(const int instr) {
+    uint16_t condFlag = (instr >> 9) & 0x7;
+    if(condFlag & reg[COND]) reg[PC] += pcoffset(instr);   
+}
+
+void Emulator:: 
+jsr(const int instr) {
+    uint16_t r1 = (instr >> 6) & 0x7;                   // first operand
+    uint16_t longFlag = (instr >> 11) & 1;
+
+    reg[R7] = reg[PC];
+    reg[PC] = longFlag ? reg[PC] + longpcoffset(instr)
+                       : reg[r1];
+}
+
+void Emulator::
+load(const int instr) {
+    uint16_t r0 = (instr >> 9) & 0x7;                   // destination register
+
+    reg[r0] = mem_read(reg[PC] + pcoffset(instr));
+    updateFlag(r0);
+}
+
+void Emulator:: 
+    loadr(const int instr) {                            // load register
+        uint16_t r0 = (instr >> 9) & 0x7;               // destination register
+        uint16_t r1 = (instr >> 6) & 0x7;               // first operand
+
+        reg[r0] = mem_read(reg[r1] + offset(instr));
+        updateFlag(r0);
+}   
+
+void Emulator:: 
+loadea(const int instr) {                               // load effective address
+    uint16_t r0 = (instr >> 9) & 0x7;                   // destination register
+    
+    reg[r0] = reg[PC] + pcoffset(instr);
+    updateFlag(r0);
+}
+
+void Emulator::
+updateFlag(uint16_t r) {
+    if(reg[r] == 0) reg[COND] = FL_ZRO;
+    else reg[COND] = (reg[r] >> 15) ? FL_NEG 
+                                    : FL_POS;
 }
--- src/Emulator.hpp
+++ src/Emulator.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 #define LC3_REGISTER_COUNT 11
 #define PC_START 0x3000
 
@@ -18,18 +19,36 @@ enum CondFlags {  FL_POS = 1 << 0, /* P */
 
 class Emulator {
     private:
-        uint16_t memory[UINT16_MAX]; // 65536 locations 
+        uint16_t memory[UINT16_MAX];             // 65536 locations 
         uint16_t reg[LC3_REGISTER_COUNT];
 
-        void add(int instr);
-
-        uint16_t signExtend(uint16_t x, int bit_count) {
-            return (x >> (bit_count - 1)) & 1) 
-                    ? x | (0xFFFF << bit_count) : x;
+        void add(const int instr);
+        void ldi(const int instr);               // load indirectly
+        void band(const int instr);              // bitwise and
+        void bnot(const int instr);              // bitwise or
+        void branch(const int instr);
+        void jump(const int instr) { 
+            uint16_t r1 = (instr >> 6) & 0x7;    // first operand
+            reg[PC] = reg[r1]; 
         }
+        void jsr(const int instr);
+        void load(const int instr);
+        void loadr(const int instr);             // load register  
+        void loadea(const int instr);            // load effective address
+
+        bool isImmMode(const int instr) { return (instr >> 5) & 0x1; }  // is it in immediate mode?
+
+        uint16_t signExtend(const int16_t x, const int bit_count) {
+            return (x >> (bit_count - 1) & 1) ? x | (0xFFFF << bit_count) : x;
+        }
+        uint16_t offset(const int instr) { return signExtend(instr & 0x3F, 6); }
+        uint16_t pcoffset(const int instr) { return signExtend(instr & 0x1FF, 9); }
+        uint16_t longpcoffset(const int instr) { return signExtend(instr & 0x7FF, 11); }
+
         void updateFlag(uint16_t r) {
             if(reg[r] == 0) reg[COND] = FL_ZRO;
-            else reg[COND] = (reg[r] >> 15) ? FL_NEG : FL_POS;
+            else reg[COND] = (reg[r] >> 15) ? FL_NEG 
+                                            : FL_POS;
         }
 
     public:

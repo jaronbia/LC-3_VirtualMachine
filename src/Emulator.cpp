5f34b83cf59d86e3bff6722a4233e8a372ffa2df
Jaron Bialecki <jaronb@Jarons-MacBook-Pro.local>
2020-06-06 21:39:58 -0400

    implemented add function and added run function too

--- src/Emulator.cpp
+++ src/Emulator.cpp
@@ -1,1 +1,39 @@
 #include "Emulator.hpp"
+
+void Emulator::
+run() {
+    bool active = true;
+    uint16_t instr;
+    uint16_t op;
+    // load args
+    // setup
+
+    reg[PC] = PC_START;
+    
+    while(active) {
+        instr = mem_read(reg[PC]++);
+        op = instr >> 12;
+
+        if(op == ADD) add(instr);
+        else cerr << "Error: Invalid command encountered" << '\n';
+    }
+    // shutdown
+}
+
+void Emulator::
+add(int instr) {
+    bool immFlag = (instr >> 5) & 0x1;      // are we in immediate mode? (originally uint16_t)
+    uint16_t r0 = (instr >> 9) & 0x7;       // destination register
+    uint16_t r1 = (instr >> 6) & 0x7;       // first operand
+
+    if(immFlag) {
+        uint16_t imm5 = signExtend(instr & 0x1F, 5);
+        reg[r0] = reg[r1] + imm5;
+    }
+    else {
+        uint16_t r2 = instr & 0x7;
+        reg[r0] = reg[r1] + reg[r2];
+    }
+
+    updateFlag(r0);
+}
--- src/Emulator.hpp
+++ src/Emulator.hpp
@@ -1,3 +1,9 @@
+#pragma once
+#define LC3_REGISTER_COUNT 11
+#define PC_START 0x3000
+
+using namespace std;
+
 enum Register { R0 = 0, R1, R2, R3, R4, 
                 R5, R6, R7, PC, COND, COUNT };
 
@@ -12,8 +18,22 @@ enum CondFlags {  FL_POS = 1 << 0, /* P */
 
 class Emulator {
     private:
-        uint16_t reg[11];
+        uint16_t memory[UINT16_MAX]; // 65536 locations 
+        uint16_t reg[LC3_REGISTER_COUNT];
+
+        void add(int instr);
+
+        uint16_t signExtend(uint16_t x, int bit_count) {
+            return (x >> (bit_count - 1)) & 1) 
+                    ? x | (0xFFFF << bit_count) : x;
+        }
+        void updateFlag(uint16_t r) {
+            if(reg[r] == 0) reg[COND] = FL_ZRO;
+            else reg[COND] = (reg[r] >> 15) ? FL_NEG : FL_POS;
+        }
 
     public:
-
+        Emulator();
+        ~Emulator() = default;
+        void run();
 };

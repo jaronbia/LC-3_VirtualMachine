0496ccbdcb8833742dcf268bc76a56959556b279
Jaron Bialecki <jaronb@Jarons-MacBook-Pro.local>
2021-02-03 03:10:19 -0500

    Added CMake file structure

---
+++ CMakeFiles/lc3.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jaronb/Projects/LC-3_VirtualMachine/src/Buffer.cpp" "/Users/jaronb/Projects/LC-3_VirtualMachine/CMakeFiles/lc3.dir/src/Buffer.cpp.o"
+  "/Users/jaronb/Projects/LC-3_VirtualMachine/src/UnixBuffer.cpp" "/Users/jaronb/Projects/LC-3_VirtualMachine/CMakeFiles/lc3.dir/src/UnixBuffer.cpp.o"
+  "/Users/jaronb/Projects/LC-3_VirtualMachine/src/VirtualMachine.cpp" "/Users/jaronb/Projects/LC-3_VirtualMachine/CMakeFiles/lc3.dir/src/VirtualMachine.cpp.o"
+  "/Users/jaronb/Projects/LC-3_VirtualMachine/src/WinBuffer.cpp" "/Users/jaronb/Projects/LC-3_VirtualMachine/CMakeFiles/lc3.dir/src/WinBuffer.cpp.o"
+  "/Users/jaronb/Projects/LC-3_VirtualMachine/src/main.cpp" "/Users/jaronb/Projects/LC-3_VirtualMachine/CMakeFiles/lc3.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
